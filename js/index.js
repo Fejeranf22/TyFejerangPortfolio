180463c08b62076323cda445c658ba8a99cbdb09
ty fejerang <fejerang.ty@gmail.com>
2017-06-12 15:41:21 -0700

    this is the initial commit of the bare-bones version os my portfolio website

---
+++ js/index.js
@@ -0,0 +1,24 @@
+// // h1 animation
+// $(document).ready(function() {
+//  $('#container').textition({
+//   handler: 'mouseenter',
+//   animation: 'ease-in-out',
+//   speed: 1
+//  });
+// });
+// $('#autoplay').textition({
+//  speed: 1,
+//  animation: 'ease-out',
+//  map: {x: 200, y: 100, z: 0},
+//  autoplay: true,
+//  interval: 1.5
+// })
+document.addEventListener("DOMContentLoaded", function(){
+  Typed.new(".element",{
+    strings:[ "First  Hello my name is Ty Fejerang :)....",
+    "...I am a Front-end Software Engineer...",
+    "...An avid coffee, tea, and life connoisseur...",
+    "...Check me out :) and Journey well..."],
+      typedSpeed: 0
+  });
+});
